90c76f4708e393546152146f366c55666537a576
Abay Darmenov <abay.darmenov@gmail.com>
2023-02-02 22:32:18 +0600

    Switching between the three modes
    Implement switching from one state of pomodoro to another once the timer
    end

--- js/timer.js
+++ js/timer.js
@@ -1,18 +1,20 @@
 import { alarm } from "./alarm.js";
+import { changeActiveBtn } from "./control.js";
 import { state } from "./state.js";
+import { addZero } from "./util.js";
 
 const minutesElem = document.querySelector('.time__minutes');
 const secondsElem = document.querySelector('.time__seconds');
 
-const showTime = (seconds) => {
+export const showTime = (seconds) => {
     let minutesText = Math.floor(seconds/60);
     let secondsText = seconds % 60;
-    minutesElem.textContent = minutesText < 10 ? '0'+minutesText: minutesText;
-    secondsElem.textContent = secondsText < 10 ? '0'+secondsText: secondsText;
+    minutesElem.textContent = addZero(minutesText);
+    secondsElem.textContent = addZero(secondsText);
 };
 
 export const startTimer = () => {
-    
+    state.timeLeft-=9;
 
     // show on the webpage
 
@@ -20,10 +22,27 @@ export const startTimer = () => {
 
     if(state.timeLeft > 0 && state.isActive){
         state.timerId = setTimeout(startTimer, 1000);
-        state.timeLeft--;
+        //TODO change this
+        
     }
     if(state.timeLeft <= 0){
-        // signal that no time is left
+        
+        if(state.status === 'work'){
+            state.activeTodo.pomodoro += 1;
+
+            if(state.activeTodo.pomodoro % state.count){
+                state.status = "break";
+            }else{
+                state.status = "relax";
+            }
+
+            
+        }else{
+            state.status = 'work';
+        }
         alarm();
+        changeActiveBtn(state.status);
+        state.timeLeft = state[state.status]*60;
+        startTimer();
     }
 };
